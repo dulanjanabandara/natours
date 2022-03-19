0e1bceb84f6b4bcc8b4ca6275995085418c1f984
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-03-19 05:54:52 +0530

    Parameter middleware was added in order to check whether the tour id is valid

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,19 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is ${val}`);
+
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID!",
+    });
+  }
+
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
 
@@ -19,14 +32,6 @@ exports.getAllTours = (req, res) => {
 
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
-
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID!",
-    });
-  }
-
   const tour = tours.find((el) => el.id === id);
 
   res.status(200).json({
@@ -53,13 +58,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID!",
-    });
-  }
-
   res.status(200).json({
     status: "success",
     data: {
@@ -69,13 +67,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID!",
-    });
-  }
-
   res.status(204).json({
     status: "success",
     data: null,

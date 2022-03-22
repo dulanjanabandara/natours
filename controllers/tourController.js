e25c77b8606a5403a51d98097eef7c0261e39c87
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-03-22 07:51:16 +0530

    Query object was created. Removes the pagination, sorting, limit and field query parameters from the URL

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,7 +6,15 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD THE QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = await Tour.find(queryObj);
+
+    // EXECUTE THE QUERY
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',

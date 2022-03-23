da1d0f5473334d2c98f95f8242318610f0f1678c
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-03-24 02:46:37 +0530

    A document middleware function was written using mongoose middlewares(pre-save hook) to add a slug according to the tour name.

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -57,6 +58,9 @@ const tourSchema = new mongoose.Schema(
       select: false, // removes the field in the query results from the schema level. Then, no need to filter when quering.
     },
     startDates: [Date],
+    slug: {
+      type: String,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -68,6 +72,25 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+// MIDDLEWARES
+// There are 4 types of middleware in mongoose,
+// 1. Document - Can act only on currently processing document
+// 2. Query
+// 3. Aggregate
+// 4. Model
+
+// pre - a DOCUMENT MIDDLEWARE. Runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// post middleware. This has the saved document as a parameter (doc).
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 

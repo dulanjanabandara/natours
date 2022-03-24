dcc0c20a749e3392a32e0d4a1a2098bf2c177d48
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-03-24 15:05:08 +0530

    Errors categorized as development and production errors. Displaying error messages was changed according to the error type

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,35 @@
+const sendErrDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.err('ERROR: ', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500; // 500 means internal server error
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrProd(err, res);
+  }
 };

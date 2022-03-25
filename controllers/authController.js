2225d65b7d2e39c325f27d5699896388e9491ffc
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-03-25 08:41:49 +0530

    User data in the signup controller changed. Now nobody can manipulate the user role when creating the user.

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,12 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   res.status(201).json({
     status: 'success',

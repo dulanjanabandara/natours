3dab1cfa14a639c43d9553494f2e19fe9faac1d1
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-04-05 17:04:00 +0530

    Sign out feature was implemented.

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import axios from 'axios';
-import {showAlert} from './alerts'
+import { showAlert } from './alerts';
 
 export const login = async (email, password) => {
   try {
@@ -21,5 +21,20 @@ export const login = async (email, password) => {
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
+  }
+};
+
+export const logout = async () => {
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://localhost:3000/api/v1/users/logout',
+    });
+
+    if (res.data.status === 'success') {
+      location.reload(true); // true - reloads from the server. not from the browser cache.
+    }
+  } catch (err) {
+    showAlert('error', 'Error logging out! Try again!');
   }
 };

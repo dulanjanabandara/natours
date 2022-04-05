d457fa989cbd2300de0a499763f5c192cf790be4
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-04-05 16:12:26 +0530

    Login alerts created and client-side javascript files bundling done.

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,52 +1,53 @@
 /* eslint-disable */
 
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations);
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiZHVsYW5qYW5hYiIsImEiOiJjbDFqM2M0dGwwdHk2M2puMnZ3NHJkY3VzIn0.-vd5HsdrDzPd3O7nRcYHgQ';
 
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/dulanjanab/cl1j4f69h007f15mq51k0uyyg',
-  scrollZoom: false,
-  //   center: [-118.113491, 34.111745],
-  //   zoom: 4,
-  //   interactive: false,
-});
+export const displayMap = (locations) => {
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoiZHVsYW5qYW5hYiIsImEiOiJjbDFqM2M0dGwwdHk2M2puMnZ3NHJkY3VzIn0.-vd5HsdrDzPd3O7nRcYHgQ';
 
-const bounds = new mapboxgl.LngLatBounds();
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/dulanjanab/cl1j4f69h007f15mq51k0uyyg',
+    scrollZoom: false,
+    //   center: [-118.113491, 34.111745],
+    //   zoom: 4,
+    //   interactive: false,
+  });
 
-locations.forEach((loc) => {
-  // Create marker
-  const el = document.createElement('div');
-  el.className = 'marker';
+  const bounds = new mapboxgl.LngLatBounds();
 
-  // Add marker
-  new mapboxgl.Marker({
-    element: el,
-    anchor: 'bottom',
-  })
-    .setLngLat(loc.coordinates)
-    .addTo(map);
+  locations.forEach((loc) => {
+    // Create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
 
-  // Add popup
-  new mapboxgl.Popup({
-    offset: 30,
-  })
-    .setLngLat(loc.coordinates)
-    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
-    .addTo(map);
+    // Add marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom',
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
 
-  // Extend the map bounds to include current location
-  bounds.extend(loc.coordinates);
-});
+    // Add popup
+    new mapboxgl.Popup({
+      offset: 30,
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+      .addTo(map);
 
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    left: 100,
-    right: 100,
-  },
-});
+    // Extend the map bounds to include current location
+    bounds.extend(loc.coordinates);
+  });
+
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100,
+    },
+  });
+};
